51cd364441a10395cbe459e437c394c9b9c45ab3
joyana133 <soa23000563@ucscollegegroup.ac.uk>
2026-05-05 10:03:06 +0100

    added main lists

--- TestDriveBookingSystem/Program.cs
+++ TestDriveBookingSystem/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestDriveBookingSystem;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Customer> customers = new List<Customer>();
+        List<Vehicle> vehicles = new List<Vehicle>();
+        List<Staff> staffList = new List<Staff>();
+        List<Booking> bookings = new List<Booking>();
+    }
+}
